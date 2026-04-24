a9355f584da3a007d47f39657a609e27703d792e
Esteban Trillo <estebandaniel779@gmail.com>
2026-04-24 00:16:28 -0300

    fix(database): corrige sintaxis de carga inicial en init.sql

--- database/init.sql
+++ database/init.sql
@@ -10,8 +10,8 @@ CREATE TABLE members (
 
 INSERT INTO members (nombre, apellido, legajo, feature, servicio, estado)
 VALUES
-('Franco', 'Oyhenart', '33555', '01', 'infraestructura-base' , 'activo')
+('Franco', 'Oyhenart', '33555', '01', 'infraestructura-base' , 'activo'),
 ('Esteban', 'Trillo', '33728', '02', 'frontend', 'activo'),
 ('Franco', 'Oyhenart', '12346', '03', 'backend', 'activo'),
-('Brenda', 'Conti', '33717', '04', 'database', 'activo')
+('Brenda', 'Conti', '33717', '04', 'database', 'activo'),
 ('Lautaro', 'Flores', '33411', '05', 'portainer', 'activo');
